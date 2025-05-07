f0741f362256876aaf09442d89defd0e545a3ab6
jobayer58 <jobayerafnan770@gmail.com>
2025-05-07 20:51:13 +0600

    update content

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault()
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value 
+        console.log(email ,password);
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col md:flex-row-reverse">
-                <div className="text-center lg:text-left">
+                <div className="text-center md:w-1/2 lg:text-left">
                     <h1 className="text-5xl font-bold">Login now!</h1>
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
                 </div>
-                <form className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <form onSubmit={handleLogin} className="card bg-base-100 md:w-1/2 max-w-sm  shadow-2xl">
                     <div className="card-body">
                         <fieldset className="fieldset">
                             <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" name='email' className="input" placeholder="Email" />
                             <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" name='password' className="input" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
-                            
                             <input className='btn btn-primary' type="submit" value={'login'} />
                         </fieldset>
                     </div>
